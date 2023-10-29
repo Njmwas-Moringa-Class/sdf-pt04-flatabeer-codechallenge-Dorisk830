486e1a4c58a41048f6f5b41702390dcbecea7fe8
DorisK830 <doris.kiprop@student.moringaschool.com>
2023-10-29 20:45:15 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const baseUrl = "http://localhost:3000";
     console.log("API base URL:", baseUrl);
-
-    // fetch beer details.
+    
+    // Function to fetch beer details
     const fetchBeer = (beerId) => {
-
         fetch(`${baseUrl}/beers/${beerId}`)
             .then((response) => response.json())
             .then((beerData) => {
-                const beerName = document.getElementById("beers-name");
-                const beerDescription = document.getElementById("beers-description");
-                const beerImage = document.getElementById("beers-image");
-                const reviewList = document.getElementById("reviews-list");
-
-                // show beer details on web
-                beerName.textContent = beers.name;
-                beerDescription.textContent = beers.description;
-                beerImage.src = beers.image_url;
-
-                //clears the existing reviews and add new reviews to the list.
+                const beerName = document.getElementById("beer-name");
+                const beerDescription = document.getElementById("beer-description");
+                const beerImage = document.getElementById("beer-image");
+                const reviewList = document.getElementById("review-list");
+    
+                beerName.textContent = beerData.name;
+                beerDescription.textContent = beerData.description;
+                beerImage.src = beerData.image_url;
+    
+                // clear existing reviews and add new reviews
                 reviewList.innerHTML = "";
                 beerData.reviews.forEach((review) => {
                     const li = document.createElement("li");
@@ -28,68 +25,65 @@ document.addEventListener("DOMContentLoaded", () => {
                 });
             });
     };
-
-    // Function to fetch and display the beer menu.
+    
+    // Function to fetch beer menu.
     const fetchBeerMenu = () => {
-        // Within this function, we initiate a fetch to obtain the list of available beers from the API.
         fetch(`${baseUrl}/beers`)
             .then((response) => response.json())
             .then((beers) => {
-
                 const beerList = document.getElementById("beer-list");
-                beerList.innerHTML = "";  // clears any existing menu items.
-
-                // Iterate through the list of beers and create a menu item for each.
+                // Clears existing menu items.
+                beerList.innerHTML = "";
+    
+                // Iterate through the list of beers.
                 beers.forEach((beer) => {
                     const li = document.createElement("li");
                     li.textContent = beer.name;
                     li.dataset.beerId = beer.id;
-
-                    // We add an event listener to each menu item to display beer details when clicked.
+    
+                    // Add an event listener to each menu item to display beer details when clicked and Add menu item.
                     li.addEventListener("click", () => {
                         fetchBeer(beer.id);
                     });
 
-                    // adds the menu item to the beer menu.
                     beerList.appendChild(li);
                 });
-
+    
+                // Select and display the first beer.
                 if (beers.length > 0) {
                     fetchBeer(beers[0].id);
                 }
             });
     };
-
-    // Add a click event listener to the review list.
+    
+    //click event listener to the review list.
     document.getElementById("review-list").addEventListener("click", (event) => {
         if (event.target.tagName === "LI") {
-            event.target.remove(); // Remove the clicked review item.
+            event.target.remove();
         }
     });
 
-    // Add a click event listener to the beer list in the navigation.
+    // Click event listener to the beer list and get beer id.
     document.getElementById("beer-list").addEventListener("click", (event) => {
         if (event.target.tagName === "LI") {
-            // Get the beer ID from the clicked item.
             const beerId = event.target.dataset.beerId;
             fetchBeer(beerId);
         }
     });
-
+    
     const reviewForm = document.getElementById("review-form");
     reviewForm.addEventListener("submit", (event) => {
-        event.preventDefault(); // We prevent the default form submission.
-
-        // retrieve new review text from the input field.
+        event.preventDefault();
+    
+        // New review
         const newReview = document.getElementById("review").value;
         const reviewList = document.getElementById("review-list");
-
-        //display the new review in the list and clear the input field.
+    
         const li = document.createElement("li");
         li.textContent = newReview;
         reviewList.appendChild(li);
-        document.getElementById("review").value = ""; // clears the review input field.
+        document.getElementById("review").value = "";
     });
-
-    fetchAndDisplayBeerMenu();
+    
+    fetchBeerMenu();
 });
